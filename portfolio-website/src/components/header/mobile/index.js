e63bd6f547e1b4dcdcdcbbeb4d6ee7985c45f9e8
tylanm <tylanmcpherson@gmail.com>
2021-07-30 11:53:36 -0700

    Implemented header options for web view

--- portfolio-website/src/components/header/mobile/index.js
+++ portfolio-website/src/components/header/mobile/index.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import './mobile.css'
 
-function Mobile() {
+function Mobile({isOpen, setIsOpen}) {
     return (
-        <div>
-            Mobile Menu
+        <div className='mobile'>
+            <div className='close-icon' onClick={() => setIsOpen(!isOpen)}>
+                <i class="ri-close-line"></i>
+            </div>
+            <div className='mobile-options'>
+                <div className="mobile-option">
+                    <a href='#projects'>
+                        <i class="ri-pencil-ruler-2-line option-icon"></i> Projects
+                    </a>
+                </div>  
+                <div className="mobile-option">
+                    <a href='#education'>
+                        <i class="ri-book-mark-line option-icon"></i> Education
+                    </a>
+                </div>  
+                <div className="mobile-option">
+                    <a href='#skills'>
+                        <i class="ri-tools-line option-icon"></i> Skills
+                    </a>
+                </div>  
+                <div className="mobile-option">
+                    <a href='#contact'>
+                        <i class="ri-user-3-line option-icon"></i> Contact
+                    </a>
+                </div>  
+            </div>
         </div>
     )
 }
--- portfolio-website/src/components/header/web/index.js
+++ portfolio-website/src/components/header/web/index.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from 'react';
+import './web.css';
 
 function Web() {
     return (
-        <div>
-            Web Menu
+        <div className="web">
+            <div className="web-option">
+                <a href='#projects'>
+                    <i class="ri-pencil-ruler-2-line option-icon"></i> Projects
+                </a>
+            </div>  
+            <div className="web-option">
+                <a href='#education'>
+                    <i class="ri-book-mark-line option-icon"></i> Education
+                </a>
+            </div>  
+            <div className="web-option">
+                <a href='#skills'>
+                    <i class="ri-tools-line option-icon"></i> Skills
+                </a>
+            </div>  
+            <div className="web-option">
+                <a href='#contact'>
+                    <i class="ri-user-3-line option-icon"></i> Contact
+                </a>
+            </div>    
         </div>
     )
 }
 
-export default Web
+export default Web;
