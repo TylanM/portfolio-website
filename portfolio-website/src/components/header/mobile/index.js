d77ef1e7157a4b4a29d84c30fd60caf1305b58e4
tylanm <tylanmcpherson@gmail.com>
2021-08-01 07:19:24 -0700

    Mobile menu now closes when a menu item is clicked

--- portfolio-website/src/components/header/mobile/index.js
+++ portfolio-website/src/components/header/mobile/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './mobile.css'
 
+
 function Mobile({isOpen, setIsOpen}) {
     return (
         <div className='mobile'>
@@ -8,22 +9,22 @@ function Mobile({isOpen, setIsOpen}) {
                 <i class="ri-close-line"></i>
             </div>
             <div className='mobile-options'>
-                <div className="mobile-option">
+                <div className="mobile-option" onClick={() => setIsOpen(!isOpen)}>
                     <a href='#projects'>
                         <i class="ri-pencil-ruler-2-line option-icon"></i> Projects
                     </a>
                 </div>  
-                <div className="mobile-option">
+                <div className="mobile-option" onClick={() => setIsOpen(!isOpen)}>
                     <a href='#education'>
                         <i class="ri-book-mark-line option-icon"></i> Education
                     </a>
                 </div>  
-                <div className="mobile-option">
+                <div className="mobile-option" onClick={() => setIsOpen(!isOpen)}>
                     <a href='#skills'>
                         <i class="ri-tools-line option-icon"></i> Skills
                     </a>
                 </div>  
-                <div className="mobile-option">
+                <div className="mobile-option" onClick={() => setIsOpen(!isOpen)}>
                     <a href='#contact'>
                         <i class="ri-user-3-line option-icon"></i> Contact
                     </a>
