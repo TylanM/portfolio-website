1d45a760177f6ee11776e75b38032eb5b6da4129
tylanm <tylanmcpherson@gmail.com>
2021-07-31 18:30:20 -0700

    Skills section implemented

---
+++ portfolio-website/src/components/data/skills.js
@@ -0,0 +1,40 @@
+export const SkillsData=[
+    {
+        type:'Frontend',
+        list: [
+            {
+                name:'ReactJS',
+                icon: <i class="devicon-react-original"></i>,
+            },
+            {
+                name:'CSS3',
+                icon: <i class="devicon-css3-plain"></i>,
+            },
+            {
+                name:'HTML5',
+                icon: <i class="devicon-html5-plain"></i>,
+            },
+        ]
+    },
+    {
+        type:'Backend',
+        list: [
+            {
+                name:'Javascript',
+                icon: <i class="devicon-javascript-plain"></i>,
+            },
+            {
+                name:'Python',
+                icon: <i class="devicon-python-plain"></i>,
+            },
+            {
+                name:'C#',
+                icon: <i class="devicon-csharp-plain"></i>,
+            },
+            {
+                name:'Java',
+                icon: <i class="devicon-java-plain"></i>,
+            }, 
+        ]
+    }
+]
