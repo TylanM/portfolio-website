db5b72d92d151d950296f3299897c25d93178c1e
tylanm <tylanmcpherson@gmail.com>
2021-07-31 17:17:34 -0700

    Project card succesfully displays project data

--- portfolio-website/src/components/body/projects/project-card.js
+++ portfolio-website/src/components/body/projects/project-card.js
@@ -5,7 +5,30 @@ function ProjectCard( {project} ) {
     return (
         <div className='project-card'>
             <div className='project-info'>
-                <label>{project.title}</label>
+                <label className='project-title'>{project.title}</label>
+                <div className="project-links">
+                    {project.demo && ( 
+                        <a className='project-link' href={project.demo}>
+                            <div className='link-button'>
+                                <i class="ri-global-line"></i>Demo
+                            </div>
+                        </a>
+                    )}
+
+                    {project.github && ( 
+                        <a className='project-link' href={project.github}>
+                            <div className='link-button'>
+                                <i class="ri-github-fill"></i>GitHub
+                            </div>
+                        </a>
+                    )}
+                </div>
+                <p>{project.about}</p>
+                <div className='project-tags'>
+                    {project.tags.map((tag) => {
+                        return  <label className='tag'>{tag}</label>;
+                    })}
+                </div>
             </div>
             <img src={project.image} className="project-photo" />
         </div>
