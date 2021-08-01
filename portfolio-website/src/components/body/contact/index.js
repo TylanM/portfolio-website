d594a6010807667f24ff2b3285e09390077f25e8
tylanm <tylanmcpherson@gmail.com>
2021-07-31 20:08:36 -0700

    Contact section implemented

--- portfolio-website/src/components/body/contact/index.js
+++ portfolio-website/src/components/body/contact/index.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import Separator from '../../common/separator'
+import SocialContact from '../../common/social-contact'
+import { SocialData } from '../../data/social'
+import './contact.css'
 
 function Contact() {
     return (
-        <div>
-         Contact Section   
+        <div className='contact'>
+         <Separator />
+         <label className='section-title'>Contact</label>
+         <div className='contact-container'>
+             <div className='contact-left'>
+                <p>Want to get in touch? Click an option below.</p>
+                <SocialContact />
+             </div>
+             <div className='download'>
+                 <a download href={require('../../../assets/resume.txt').default}>
+                 <i class="ri-file-download-line download-icon"></i>
+                     Download Resume
+                 </a>
+             </div>
+
+         </div>
         </div>
     )
 }
